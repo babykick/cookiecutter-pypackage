2ad5dd36f1ea08514426dd4ed33057312b3b9514
babykick <babykick@163.com>
2020-05-18 16:36:29 +0800

    add condition

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -1,25 +1,12 @@
-import argparse
-
+{%- if cookiecutter.command_line_interface == "Click" -%}
 import click
+{%- elif cookiecutter.command_line_interface == "argparse" -%}
+import argparse
+{% endif %}
 
-# from .core import xxx
+from .core import *
 
-#-- Use argparse --#
-
-# def _argparse():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('url', nargs='?', help='target playlist url')
-#     parser.add_argument('--listfile', help='file containing video links')
-#     parser.add_argument('--savedir', default=cur_dir, help='directory downloaded video saved')
-#     parser.add_argument('--useindex', action='store_true', help='use index or not in format')
-#     parser.add_argument('--displayid', action='store_true', help='add display id in output template')
-    
-#     return parser.parse_args()
-# args = _argparse()
-
-
-#-- Use click --#
-
+{% if cookiecutter.command_line_interface == "Click" %}
 @click.command()
 @click.argument('arg1')
 @click.option('-o', '--opt1', default='', help='...', )
@@ -28,7 +15,22 @@ def main(arg1, opt1):
     click.echo("Replace this message by putting your code into "
                "{{cookiecutter.project_slug}}.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
+{% elif cookiecutter.command_line_interface == "argparse" %}
+def _argparse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', nargs='?', help='target playlist url')
+    parser.add_argument('--listfile', help='file containing video links')
+    parser.add_argument('--savedir', default=cur_dir, help='directory downloaded video saved')
+    parser.add_argument('--useindex', action='store_true', help='use index or not in format')
+    parser.add_argument('--displayid', action='store_true', help='add display id in output template')
+    
+    return parser.parse_args()
+args = _argparse()
 
+
+def main():
+    ...
+{% endif %}
 
 if __name__ == "__main__":
     main()
